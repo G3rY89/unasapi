9b931db3547290c70f327e5a97186ac044ca5b82
kudog <kudo.gergely@kulcs-soft.hu>
2019-09-23 15:09:08 +0200

    Revert "Eureka discovery removed"
    This reverts commit a5ff2c2b7c6f347799ba5cce16a3ff883ce13418.

--- src/main/java/com/ks/supersync/unasapi/UnasapiApplication.java
+++ src/main/java/com/ks/supersync/unasapi/UnasapiApplication.java
@@ -2,9 +2,11 @@ package com.ks.supersync.unasapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UnasapiApplication {
 
